6a82869207e31f553981a959012cf95a95939d55
virginioTenorio <eliastenoriov@hotmail.com>
2022-01-13 08:52:17 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,12 +22,15 @@ public class Program {
 		          System.out.println();
 		          System.out.print("Source: ");
 		          ChessPosition source = UI.readChessPosition(sc);
-				
-				  System.out.println();
-				  System.out.print("Target: ");
-				  ChessPosition target = UI.readChessPosition(sc);
-				  
-				  ChessPiece capChessPiece = chessMatch.performChessMove(source, target);
+		          
+		          boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+		          UI.clearScreen();
+		          UI.printBoard(chessMatch.getPieces(), possibleMoves);
+		          System.out.println();
+		          System.out.print("Target: ");
+		          ChessPosition target = UI.readChessPosition(sc);
+		          
+		          ChessPiece capChessPiece = chessMatch.performChessMove(source, target);
 			}	
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
@@ -39,5 +42,6 @@ public class Program {
 			}
 		}
 	}
-
+	
 }
+				
