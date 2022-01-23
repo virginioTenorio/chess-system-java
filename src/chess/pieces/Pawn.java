f9e95450476091eb5fbe9cd1094b524c3656d902
virginioTenorio <eliastenoriov@hotmail.com>
2022-01-23 13:09:08 -0300

    Queen

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -15,9 +15,8 @@ public class Pawn extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-
+		
 		Position p = new Position(0, 0);
-
 		if (getColor() == Color.WHITE) {
 			p.setValues(position.getRow() - 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)) {
@@ -44,7 +43,8 @@ public class Pawn extends ChessPiece{
 			}
 			p.setValues(position.getRow() + 2, position.getColumn());
 			Position p2 = new Position(position.getRow() - 1, position.getColumn());
-			if (getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().ThereIsAPiece(p2) && getMoveCount() == 0) {
+			Position p21 = new Position(position.getRow() + 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p) && getBoard().positionExists(p21) && !getBoard().ThereIsAPiece(p21) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 1, position.getColumn() - 1);
@@ -58,12 +58,11 @@ public class Pawn extends ChessPiece{
 		}
 		return mat;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "P";
 	}
-
-}
 	
+}
 
